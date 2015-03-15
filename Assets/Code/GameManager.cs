da638872568225b1807272b67e6c001a52c9f13f
Rohit Moni <rohitmdxb@gmail.com>
2015-03-15 15:23:19 -0400

    Touch input now works

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -1,13 +1,22 @@
-﻿using Assets.Code;
+﻿using System.Net.Mime;
+using Assets.Code;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    private static Text _debugText;
 
 	// Use this for initialization
 	void Start () {
 	    CreateCoins();
 	}
+
+    void Awake()
+    {
+        _debugText = GameObject.Find("DebugText").GetComponent<Text>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,5 +26,10 @@ public class GameManager : MonoBehaviour {
     private void CreateCoins()
     {
         CoinManager.CreateCoinAt(new Vector3(2, 4, 0));
+    }
+
+    public static void Debug(string text)
+    {
+        _debugText.text = text;
     }
 }
--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour {
 
     /* Properties */
     private bool _canInput;
+    private bool _isTouching;
+    private float _distToGround;
     private float _inputHoldTime;
     private float _cooldownTimer;
     private int _numberOfCoins;
@@ -21,10 +23,14 @@ public class Player : MonoBehaviour {
     void Awake()
     {
         _canInput = true;
+        _isTouching = false;
+        _distToGround = GetComponent<Collider>().bounds.extents.y;
+        
         _inputHoldTime = _cooldownTimer = 0;
         _rb = GetComponent<Rigidbody>();
         _rb.velocity = Vector3.zero;
         _numberOfCoins = 0;
+
     }
 
 	// Update is called once per frame
@@ -37,10 +43,11 @@ public class Player : MonoBehaviour {
         if (!_canInput)
 	        _cooldownTimer += Time.smoothDeltaTime;
 
-	    if (_cooldownTimer >= JumpCooldown)
+	    if (_cooldownTimer >= JumpCooldown && IsGrounded())
 	    {
+            GameManager.Debug(_cooldownTimer.ToString());
 	        _canInput = true;
-	        _cooldownTimer = 0;
+            _cooldownTimer = 0;
 	    }
 	}
 
@@ -70,29 +77,33 @@ public class Player : MonoBehaviour {
 
     private void HandleTouchInput()
     {
-        foreach (var touch in Input.touches)
+        if (Input.touchCount >= 1)
         {
-            if (touch.phase != TouchPhase.Canceled || touch.phase != TouchPhase.Ended)
+            _isTouching = true;
+            var touch = Input.touches[0];
+
+            if (touch.rawPosition.x <= Screen.width / 2f)
             {
-                if (touch.rawPosition.x <= Screen.width/2f)
-                {
-                    _inputHoldTime -= Time.smoothDeltaTime;
+            _inputHoldTime -= Time.smoothDeltaTime;
 
-                    _inputHoldTime = Math.Max(_inputHoldTime, -MaximumInputHoldTime);
-                    _inputHoldTime = Math.Min(_inputHoldTime, -MinimumInputHoldTime);
-                }
-                else
-                {
-                    _inputHoldTime += Time.smoothDeltaTime;
-
-                    _inputHoldTime = Math.Max(_inputHoldTime, MinimumInputHoldTime);
-                    _inputHoldTime = Math.Min(_inputHoldTime, MaximumInputHoldTime);
-                }
+            _inputHoldTime = Math.Max(_inputHoldTime, -MaximumInputHoldTime);
+            _inputHoldTime = Math.Min(_inputHoldTime, -MinimumInputHoldTime);
             }
-            else if (_canInput)
+            else
             {
+                _inputHoldTime += Time.smoothDeltaTime;
+
+                _inputHoldTime = Math.Max(_inputHoldTime, MinimumInputHoldTime);
+                _inputHoldTime = Math.Min(_inputHoldTime, MaximumInputHoldTime);
+            }
+        }
+        else if (_isTouching)
+        {
+            // Touch release
+            _isTouching = false;
+
+            if (_canInput)
                 SetRbVelocity();
-            }
         }
     }
 
@@ -101,11 +112,19 @@ public class Player : MonoBehaviour {
         // We add to velocity here
         _rb.velocity = new Vector3(Mathf.Sign(_inputHoldTime), Math.Abs(_inputHoldTime) * 3, 0) * 5;
         _inputHoldTime = 0;
+        _cooldownTimer = 0;
         _canInput = false;
     }
 
     public void CollectCoin()
     {
         _numberOfCoins++;
+    }
+
+    private bool IsGrounded()
+    {
+        var _collider = GetComponent<Collider>();
+
+        return Physics.CheckCapsule(_collider.bounds.center,new Vector3(_collider.bounds.center.x,_collider.bounds.min.y-0.1f,_collider.bounds.center.z),0.18f);
     }
 }
